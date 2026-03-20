d189c696f3b23103bc6393cc06498873c2fcbca4
aijanvi52-cell <aijanvi52@gmail.com>
2026-03-21 02:07:56 +0530

    create/update file

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>JS Career</h1>
+      <p>This is a modern and professional website</p>
+    </div>
+  );
+};
+
+export default Hero;
